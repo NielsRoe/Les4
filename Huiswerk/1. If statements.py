73a59c1ded81f21d98295fb660422b38e8126d33
NielsRoe <43231324+NielsRoe@users.noreply.github.com>
2018-09-27 10:40:57 +0200

    Delete 1. If statements.py

--- Huiswerk/1. If statements.py
+++
@@ -1,7 +0,0 @@
-score = int(input("Geef je score: "))
-
-if score > 15 or score == 15:
-    print("Gefeliciteerd!")
-    print("Met een score van %d ben je geslaagd!" % (score))
-else:
-    print("Helaas, je score is te laag.")
