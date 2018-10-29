b7f2a28453b623355d54f1c8660ec7801b2b827d
NielsRoe <43231324+NielsRoe@users.noreply.github.com>
2018-10-29 09:34:58 +0100

    Delete 5. String Functions.py

--- Huiswerk/5. String Functions.py
+++
@@ -1,7 +0,0 @@
-def gemiddelde():
-    woorden = input("Vul een willekeurige zin in: ").split(" ")
-    som = 0
-    for woord in woorden:
-        som += len(woord)
-    print("Gemiddelde aantal karakters per woord: %.1f" % (som / len(woorden)))
-gemiddelde()
